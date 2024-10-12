e592fd74f6b473821c2c4cff9f438b63a3bafbe7
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-10-12 18:42:26 +0700

    Main

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello\fWorld");
         try {
             FileReader reader = new FileReader("input.txt");
             TermProjectLexer lexer = new TermProjectLexer(reader);
